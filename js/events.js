f3ef55a44d0c01026443c3e6bec35ae32fac1145
Jess <jess.harrelson0707@gmail.com>
2016-11-06 14:45:31 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
+function getIt(){
+  $('p').on("click", function(){
+    alert('Hey!');
+});
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $(document).on("keydown", function(key){
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 //define functions here
 
 $(document).ready(function(){
-
+  return getIt()
+  frameIt()
+  return pressIt()
+  // return submitIt()
 // call functions here
 
 });
